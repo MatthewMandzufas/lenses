35e2a85c8c9a1b89809621ce28f391d069eb9b1f
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-08-07 12:55:35 +0800

    RED: lenses › view should retrieve the value using the lens

---
+++ src/lenses.spec.ts
@@ -0,0 +1,30 @@
+const user = { name: 'Alice', address: { city: 'Wonderland', zip: '12345' } };
+
+type Address = {
+    city: string;
+    zip: string;
+};
+
+type User = {
+    name: string;
+    address: Address;
+};
+
+describe(`lenses`, () => {
+    it(`view should retrieve the value using the lens`, () => {
+        const nameLens = lens<User, string>(
+            (user) => user.name,
+            (newName, user) => ({ ...user, name: newName })
+        );
+        const cityLens = lens<User, string>(
+            (user) => user.address.city,
+            (newCity, user) => ({
+                ...user,
+                address: { ...user.address, city: newCity },
+            })
+        );
+
+        expect(view(nameLens, user)).toBe('Alice');
+        expect(view(cityLens, user)).toBe('Wonderland');
+    });
+});
