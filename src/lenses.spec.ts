b0be75d9a0ddf2c75a5b3c7d817443036f35732f
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-08-14 11:33:21 +0800

    RED: lenses › set should update the value using the lenS

--- src/lenses.spec.ts
+++ src/lenses.spec.ts
@@ -13,20 +13,24 @@ type User = {
 };
 
 describe(`lenses`, () => {
+    const nameLens = lens<User, string>(
+        (user) => user.name,
+        (newName, user) => ({ ...user, name: newName })
+    );
+    const cityLens = lens<User, string>(
+        (user) => user.address.city,
+        (newCity, user) => ({
+            ...user,
+            address: { ...user.address, city: newCity },
+        })
+    );
     it(`view should retrieve the value using the lens`, () => {
-        const nameLens = lens<User, string>(
-            (user) => user.name,
-            (newName, user) => ({ ...user, name: newName })
-        );
-        const cityLens = lens<User, string>(
-            (user) => user.address.city,
-            (newCity, user) => ({
-                ...user,
-                address: { ...user.address, city: newCity },
-            })
-        );
-
         expect(view(nameLens, user)).toBe('Alice');
         expect(view(cityLens, user)).toBe('Wonderland');
+    });
+    it(`set should update the value using the lens`, () => {
+        const renamedUser = set(nameLens, 'Gerald', user);
+        expect(view(nameLens, renamedUser)).toBe('Gerald');
+        expect(renamedUser).not.toBe(user);
     });
 });
